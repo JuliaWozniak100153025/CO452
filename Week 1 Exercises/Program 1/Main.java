9b7e1ef7ef70d4dcf5cb1ffb27ade02f4dc08dd5
JuliaWozniak100153025 <100153025@student.burnley.ac.uk>
2022-09-30 10:06:42 +0100

    Program2_upload

--- Week 1 Exercises/Program 1/Main.java
+++ Week 1 Exercises/Program 1/Main.java
@@ -3,7 +3,7 @@ class Main
 
     public static void main(String[] args)
 {
-    System.out.println("Hello Julia!");
+    System.out.println("Hello World");
 }
 
 
---
+++ Week 1 Exercises/Program 2/Main.java
@@ -0,0 +1,12 @@
+//package Week 1 Exercises.Program 2;
+
+class Main
+{       
+
+    public static void main(String[] args)
+{
+    System.out.println("Hello Julia!");
+}
+
+
+}
