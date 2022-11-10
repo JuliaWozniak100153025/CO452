b8d32f1ee6aa6f2fe89cb20b5e4bcfadeab4b44a
JuliaWozniak100153025 <100153025@student.burnley.ac.uk>
2022-11-10 19:55:56 +0000

    UpdateSix

--- CW1Assignment/Main.java
+++ CW1Assignment/Main.java
@@ -1,5 +1,13 @@
 package CW1Assignment;
 
+/**
+ * Main program should be run in testing.
+ *
+ * 
+ * @author     Julia Wozniak
+ * @version    0.1 (10.11.2022)
+ */
+
 class Main 
 {
     
@@ -11,14 +19,15 @@ class Main
   }
 
   /**
-    Method to build sample songs and test they are stored
-    @return - playlist arraylist of songs.
+    Method used to build sample songs and test they are stored.
+    @return - An array list of songs called SongCollction.
   */
   public static SongCollection buildSongCollection(SongCollection mySongCollection)
   {   
     Song[] songs = new Song[10];
     
-    // Create 10 songs for playlist
+   /* Creating 10 songs for playlist in a [0] to [9] array. */ 
+
     songs[0] = new Song("Rum Rage","Sticky Fingers",12);
     songs[1] = new Song("Ghosts","Bakar",23);
     songs[2] = new Song("The Neighbourhood","The Beach",15);
@@ -30,7 +39,9 @@ class Main
     songs[8] = new Song("Arabella","Arctic Monkeys",33);
     songs[9] = new Song("Wonderwall","Oasis",18);
 
-    // Create playlist with the sample songs
+   /* Builidng a song collection with the songs created in the array list above.
+    * Using a for loop to print the array list objects from 0 to 9.
+    */
     
     for (int loop=0;loop<=9;loop++)
       {
@@ -53,84 +64,100 @@ class Main
         catch(Exception e){}
   }
 
-
-
   /**
-    Method: to display the latest playlist
-    @return void so no return value required
+    Method: to display the latest song collection.
+    @return No return is needed, void.
   */
   public static void latestSongCollection(SongCollection mySongCollection)
   {
     System.out.println("---------------");
-    System.out.println("Latest Playlist");
-    System.out.println("---------------");
+    System.out.println("Updated Song Collection");
+    System.out.println("======================");
     System.out.println("");
     mySongCollection.printHeader();
     mySongCollection.printSongs();
     System.out.println("");
   }
+
+  /* Main method menu created within the application- displaying a menu with 5 options to user. */
   
   public static void menu(SongCollection mySongCollection)
   {
-    int track; //Used to store selection of track
+     /* Used to store selection of track. */
+
+    int track,plays;
     String[] options = {"[1] Add a new song to the list of songs",
                             "[2] Remove a song from list of songs",
                             "[3] Print a list of all the songs stored",
                             "[4] Print a list of songs over a given number of plays",
                             "[0] Exit",
                        };
-    //initialise option
+    /* Initialise option. */
     int choice = 1;
     while (choice !=0)
     {
-      //Display basic menu header
+      /* Display basic menu header. */
+
       System.out.println("");
-      System.out.println("Playlist Menu");
-      System.out.println("=============");
+      System.out.println("Song Collection Menu");
+      System.out.println("====================");
       
-      //Display options
+      /* Display options to user. */
+
       for (String option : options)
       {
         System.out.println(option);
       }
       choice = InputReader.getInt("Please make your choice: ");
        
-    //Execute decisions from menu for the range of menu options        
+    /* Execute decisions from menu for the range of menu options. */
+
       switch(choice)
       {
+        /* Adding a song into the Song Collection array. Input Reader used for user input. */
         case 1:
           String nameOfSong = InputReader.getString("Please enter Song Title: ");
           String theArtist = InputReader.getString("Please enter Artist: ");
           int plyCount = InputReader.getInt("Please enter Playcount: ");
           Song song = new Song(nameOfSong,theArtist,plyCount);
           mySongCollection.addSong(song);
-          break;
+          break; 
+
+          /* Deleting a song from the Song Collection.  */
           
         case 2:
           latestSongCollection(mySongCollection);
-          track = InputReader.getInt("Select id of track to delete: ");
+          track = InputReader.getInt("Select ID of the track to delete: ");
           mySongCollection.deleteSong(track-1);
           System.out.println("xxxxxx Track Deleted xxxxxx");
           break;
-          
+
+          /* Prints the latest Song Collection. Press key allows to run menu again.*/  
+        
         case 3:
           latestSongCollection(mySongCollection);
           pressKey();
           break;
           
+          /* Print the songs over the inputted number of plays. Press key allows to run menu again. */
+
         case 4:
-          track = InputReader.getInt("Enter number of plays: ");
+          plays = InputReader.getInt("Enter the number of plays: ");
           mySongCollection.printHeader();
-          mySongCollection.printSelectSongs(track);
+          mySongCollection.printSelectSongs(plays);
           pressKey();
           break;
+
+          /* Exit the menu. Menu will run again. */
           
         case 0:
           System.out.println("Bye....");
           break;
           
+        /* Any other value input will be dismissed and line below will print. Menu will not run again. */
+
         default:
-          System.out.println("Not a valid options, try again...");
+          System.out.println("Not a valid option, try again please.");
           break;
       }
     }
