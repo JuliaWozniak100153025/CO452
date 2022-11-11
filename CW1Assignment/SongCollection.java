8e96628654b004279b713a08714f4e09ef8cbfba
JuliaWozniak100153025 <100153025@student.burnley.ac.uk>
2022-11-11 00:15:06 +0000

    UpdateSeven

--- CW1Assignment/SongCollection.java
+++ CW1Assignment/SongCollection.java
@@ -1,8 +1,12 @@
 package CW1Assignment;
-
 import java.util.ArrayList;
+
 /**
- Class which manages a playlist of songs.
+ * Class Song Collection - array list created, all menu option methods created.
+ *
+ * 
+ * @author     Julia Wozniak
+ * @version    0.1 (01.11.2022)
  */
 
 public class SongCollection  
@@ -17,8 +21,8 @@ public class SongCollection
   }
 
   /**
-  Method to get size of playlist
-  @return void
+  Method to get size of the Song Collection.
+  @return void - There is no return of any type, as void keyword is used.
   */
   public int songCollectionSize()
   {
@@ -26,20 +30,21 @@ public class SongCollection
   }
 
 
- /* The 'add' method to add the song to the array of songs */
+ /* The 'add' method to add the song to the array of songs. */
  
  public void addSong(Song song)
     {
       mySongCollection.add(song);
     }
 
-    /* The 'remove' method to delete the song from the array of songs */
+    /* The 'remove' method to delete the song from the array of songs. */
 
 public void deleteSong(int id)
     {
       mySongCollection.remove(id);
     }
 
+/* Method used to print the list of songs in the array. */
 
 public void printSongs()
 {
@@ -52,27 +57,23 @@ public void printSongs()
 }
 
 /**
-Method: Prints a header for table of songs - needs attention! 
-@return void
+Method: Prints a header for the table of songs.
 */
 
 public void printHeader()
-{
-  System.out.printf("--------------------------------%n");
-  System.out.printf("| %-10s | %-8s | %4s |%n", "Song Name","Artist Name","Play Count");
-  System.out.printf("--------------------------------%n");
-}
+{}
 
 /**
-Method: Prints selected songs over a specified playcount
-@return void
+Method: Prints selected songs over a specified number of times played.
+@return void - - There is no return of any type, as void keyword is used.
 */
-public void printSelectSongs(int playCount)
+
+public void printSelectSongs(int numOfPlays)
 {
   for (int index = 0; index < mySongCollection.size(); index++)
     {        
       Song s = mySongCollection .get(index);
-      if (s.getPlayCount() > playCount)
+      if (s.getnumberOfPlays() > numOfPlays)
       {
           System.out.print("["+(index+1)+"]");
           s.printSong();
